16066a4b082f79a2923d78a65eda56df05727b03
Natanael <NatanaelUF17@gmail.com>
2021-10-26 15:59:35 -0400

    👨🏽‍💻 initial project commit

---
+++ SwifUI-Weather/SwifUI-Weather/SwifUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUI_WeatherApp.swift
+//  SwifUI-Weather
+//
+//  Created by Natanael Urena on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct SwifUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
